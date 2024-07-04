355ae33a906fdd26ea72a6a403bad9421b52ddb9
sandeshpatil31 <sandeshpatil.3188@gmail.com>
2024-07-04 18:09:37 +0530

    addtion user input

---
+++ Program3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int a=0;
+	int b=0;
+	int c=0;
+
+     printf("Enter first number: \n");
+     scanf("%d",&a);
+
+     printf("Enter second number: \n");
+     scanf("%d",&b);
+
+
+	c=a+b;
+	printf("Addition is = %d \n",c);
+
+	return 0;
+}
