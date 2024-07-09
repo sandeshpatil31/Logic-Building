3a98cc54a3efda342ca25d3177c92be743265515
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-07-09 18:27:16 +0530

    Create Program28.c

---
+++ Program28.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <stdbool.h>
+bool checkMultiple(int iNum)
+{
+	if (iNum%3==0 && iNum%5==0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+int main()
+{
+	int iNum=0;
+	bool bResult=false;
+	printf("Enter the number:\n");
+	scanf("%d",&iNum);
+
+	bResult=checkMultiple(iNum);
+	if (bResult==true)
+	{
+		printf("Number is multiple of 3 and 5\n");
+	}
+	else
+	{
+		printf("Number is not multiple of 3 and 5\n");
+	}
+
+	return 0;
+}
