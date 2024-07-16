9f410eb67f7b26f3aab4753db7957d9d8e80f9f3
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-07-16 17:18:40 +0530

    get exam tine using Switch

---
+++ Program31.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void getExamTimeTable(int iClass)
+{
+	switch(iClass)
+	{
+	case 1: 
+		printf("Your exam is at 9am\n");
+		break;
+
+	case 2:
+		printf("Your exam is at 10am\n");
+		break;
+
+	case 3:
+		printf("Your exam is at 11am\n");
+		break;
+
+	case 4:
+		printf("Your exam is at 12pm\n");
+		break;
+		
+	default:
+		printf("Wrong Input");
+	}
+}
+int main()
+{
+	int iClass = 0;
+	printf("Enter your class\n");
+	scanf("%d", &iClass);
+
+	getExamTimeTable(iClass);
+	return 0;
+}
