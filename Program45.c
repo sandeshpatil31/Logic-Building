d7320ba166461632acfe33245b8f919df2adb539
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-07-16 17:27:22 +0530

    check prime

---
+++ Program45.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool checkPrime(int iNum)
+{
+	int iCnt = 0;
+	bool bFlag = true;
+	for(iCnt=2;iCnt<iNum;iCnt++)
+	{
+		if(iNum%iCnt==0)
+		{
+			bFlag = false;
+		}
+	}
+	return bFlag;
+}
+
+int main()
+{
+	int iNum = 0;
+	bool bRet = false;
+
+	printf("Enter any number:\n");
+	scanf("%d", &iNum);
+
+	bRet = checkPrime(iNum);
+
+	if(bRet == true)
+	{
+		printf("%d is  prime number\n", iNum);
+	}
+	else
+	{
+		printf("%d is non  prime number\n", iNum);
+	}
+}
