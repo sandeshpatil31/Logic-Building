4fe6fc32c98e5839b01a145bd853a596cf2aafc2
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-07-16 17:17:21 +0530

    check multiple 3 and 5

--- Program28.c
+++ Program28.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <stdbool.h>
+
 bool checkMultiple(int iNum)
 {
 	if (iNum%3==0 && iNum%5==0)
@@ -15,10 +16,13 @@ int main()
 {
 	int iNum=0;
 	bool bResult=false;
+
 	printf("Enter the number:\n");
 	scanf("%d",&iNum);
 
+
 	bResult=checkMultiple(iNum);
+	
 	if (bResult==true)
 	{
 		printf("Number is multiple of 3 and 5\n");
