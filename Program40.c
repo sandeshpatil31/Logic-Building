596dfe952e42cfcdb8b40f0a5022268f6bcc7283
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-07-16 17:24:28 +0530

    solid principle S
    single responsibility principle

---
+++ Program40.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int SumOfFactors(int iNum)
+{
+  
+	int iCnt = 0;
+	int iSum = 0;
+	for(iCnt=1;iCnt<=iNum/2;iCnt++)
+	{
+		if(iNum%iCnt==0)
+		{
+			iSum = iSum+iCnt;
+		}
+		
+	return iSum;
+	
+}
+
+bool checkPerfectNumber(int iNum)
+{
+	int iSum=0;
+	iSum=SumOfFactors(iNum);
+    if(iSum == iNum)
+    {
+    	return true;
+    }
+    else
+    {
+    	return false;
+    }
+}
+int main()
+{
+	int iNum = 0;
+	bool bResult=false;
+
+	printf("Enter number:\n");
+	scanf("%d", &iNum);
+
+	bResult=checkPerfectNumber(iNum);
+
+	if(bResult==true)
+	{
+		printf("Number is perfect number");
+	}
+	else
+	{
+		printf("Number is not perfect number");
+	}
+	
+	return 0;
+}
