24ec85a0b830f3b10274f5593185369815874822
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-07-20 08:28:17 +0530

    check prime using solid principle

---
+++ Program54.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int countFactors(int iNum)
+{
+   int iCnt=0;
+   int iFactCount=0;
+   for(iCnt=2;iCnt<=iNum/2;iCnt++)
+   {
+      if (iNum%iCnt==0)
+      {
+       iFactCount++;
+         break;
+        }
+      
+   }
+   return iFactCount;
+}
+
+bool checkPrime(int iNum)
+{
+
+    int iFactCount=0;
+  iFactCount=countFactors(iNum);
+   
+   if (iFactCount==0)
+   {
+      return true;
+   }
+   else
+   {
+      return false;
+   }
+    
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+
+int main()
+{
+	int iNum=0;
+     bool bRet=false;
+
+     printf("Enter the number:\n");
+     scanf("%d",&iNum);
+
+     bRet=checkPrime(iNum);
+
+     if(bRet==true)
+     {
+     	printf("prime number");
+     }
+     else
+     {
+     	printf("not prime number");
+     }
+     
+	return 0;
+}
