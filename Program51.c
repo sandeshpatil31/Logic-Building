437e32833041805fd904e0388c964038081bdf1a
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-07-20 08:26:40 +0530

    check prime using factcount

---
+++ Program51.c
@@ -0,0 +1,37 @@
+/* Problem Statement - Write a program to return the count of the total number of
+factors of a given number to Excluding 1 and and given number */
+ 
+#include<stdio.h>
+
+int countFactors(int iNum)          // calling function
+{
+
+	 int iCnt=0;
+	 int iFactCount=0;
+	 for (int iCnt = 2; iCnt<=iNum/2; iCnt++)
+	 {
+	 	if(iNum%iCnt==0)
+	 	{
+	 		iFactCount++;
+		}
+
+	 }
+	 
+	 return iFactCount ;                 // return factCount to main caller fun
+
+}
+
+int main()
+{
+	int iNum=0;
+	int iFactCount=0;
+
+	printf("Enter the Number:\n");  
+	scanf("%d",&iNum);                  // accpet user input
+
+	iFactCount=countFactors(iNum);    // caller function 
+
+	printf("factor count= %d\n",iFactCount);
+
+	return 0;
+}
