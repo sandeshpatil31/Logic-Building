63bb8b798f00666707a19827e8554b7af0879e6f
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-08-04 20:39:50 +0530

    Header file calculate power

---
+++ header70.h
@@ -0,0 +1,5 @@
+// create header file to import in business file and main file 
+
+#include <stdio.h>
+typedef unsigned long int ULONG;  // use to refrence 
+ULONG calculatePower(int,int);    //  business fun forward delcare
