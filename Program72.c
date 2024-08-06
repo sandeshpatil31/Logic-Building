ce28aa727e0d1d45fcdfdab29b1c0998d0c2f098
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-08-06 09:46:57 +0530

    Display n numbers

---
+++ Program72.c
@@ -0,0 +1,27 @@
+/* Problem Statement :- To accept n number from the user and display numbers*/
+
+#include<stdio.h>
+int main()
+{
+	int iNum1=0;
+	int iNum2=0;
+	int iNum3=0;
+	int iNum4=0;
+	int iNum5=0;
+
+	printf("Enter any 5 integer numbers:\n");
+	scanf("%d",&iNum1);
+	scanf("%d",&iNum2);
+	scanf("%d",&iNum3);
+	scanf("%d",&iNum4);
+	scanf("%d",&iNum5);
+
+	printf("Elements are:\n");
+	printf("%d\n",iNum1);
+	printf("%d\n",iNum2);
+    printf("%d\n",iNum3);
+    printf("%d\n",iNum4);
+    printf("%d\n",iNum5);
+
+
+}
