5992a98ddcdd4bd0637b401db836580a1ca1de8b
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-08-06 09:48:55 +0530

    array using pointer

---
+++ Program75.c
@@ -0,0 +1,30 @@
+/* Problem statement :- To accept 5 numbers From user and display inputed number using array using business function( use pointer) */ 
+
+#include<stdio.h>
+
+void display(int * iPtr )
+{
+      
+      printf("Array Elements are:\n");
+      int iCnt=0;
+      for(iCnt=0;iCnt<=4;iCnt++)
+      {
+          printf("%d\n",*iPtr);
+          iPtr++;   // Pointer  Arithmatic
+      }
+}
+
+int main()
+{
+      int iArr[5];
+      printf("Enter array Elements:\n");
+
+      int iCnt=0;
+      for(iCnt=0;iCnt<=4;iCnt++)
+      {
+          scanf("%d",&iArr[iCnt]);
+      }
+
+      display(iArr);   // call by address
+      return 0;
+}
