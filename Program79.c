706a38b72277c9cc80d39b6208501927a353752a
Sandesh Patil <115943497+sandeshpatil31@users.noreply.github.com>
2024-08-06 10:11:56 +0530

    Create Program79.c

---
+++ Program79.c
@@ -0,0 +1,30 @@
+/* Problem statement :- To accept 5 numbers From user and display inputed number using array  */ 
+
+#include<stdio.h>
+
+void display(int iPtr [])
+{
+      
+      printf("Array Elements are:\n");
+      int iCnt=0;
+      for(iCnt=0;iCnt<=4;iCnt++)
+      {
+          printf("%d\n",iPtr[iCnt]);
+         
+      }
+}
+
+int main()
+{        
+      int iArr[5];
+      printf("Enter array Elements:\n");
+
+      int iCnt=0;
+      for(iCnt=0;iCnt<=4;iCnt++)
+      {
+          scanf("%d",&iArr[iCnt]);
+      }
+
+      display(iArr);   // call by address
+      return 0;
+}
